a862a0fa52795c81e26de31ade890e7453a33bc0
Toniorganizer <fahtony2608@gmail.com>
2024-07-17 13:44:18 +0700

    rev-1

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/ngipo-vue/',
+  assetsInclude: ['**/*.JPG'],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
